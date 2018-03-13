7ef45019624c364c958cdacd8e91efd8068407af
Unknown <genroa@hotmail.fr>
2018-03-13 19:59:11 +0100

    fix script packaging

--- package_app.bat
+++ package_app.bat
@@ -1,2 +1,2 @@
 cd resources\app
-npm install & cd ..\.. & asar pack resources\app resources\app.asar
+npm install & cd ..\.. & asar pack resources\app resources\app.asar & ECHO "Packaging termine!"
